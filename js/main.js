32080d5116d708d005e7eae0b1c6d35be2b57c83
Your Name <thomas.sanichchung@gmail.com>
2022-05-13 19:42:44 +0200

    maj

--- js/main.js
+++ js/main.js
@@ -13,9 +13,3 @@ document.addEventListener("DOMContentLoaded", e => {
     });
 
 });
-
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-
-btn1.onclick = () => document.body.style.backgroundColor = "#2F2F2F";
-btn2.onclick = () => document.body.style.backgroundColor = ;
